61991f6da1e0843036e99a2056c7164f8085236b
Marco Della Rocca <marco741147@gmail.com>
2022-11-10 12:05:18 +0100

    read data from single edf

--- scripts/dataset/generate_h5.py
+++ scripts/dataset/generate_h5.py
@@ -15,34 +15,61 @@ def load_slices_metadata(output_path) -> dict[str, dict[str, list[list]]]:
         return json.load(f)
 
 
-def extract_data(edf_path: pathlib.Path) -> np.ndarray:
-    # raw_edf = mne.io.read_raw_edf(edf_path, include=config.USEFUL_CHANNELS, stim_channel="")
-    raw_edf = mne.io.read_raw_edf(edf_path, preload=True)
-    print(edf_path)
-    return
-    print(raw_edf.ch_names)
-    print("Missing:")
-    for channel in config.USEFUL_CHANNELS:
-        if channel not in raw_edf.ch_names:
-            print(channel)
-    print("extra")
-    for channel in raw_edf.ch_names:
-        if channel not in config.USEFUL_CHANNELS:
-            print(channel)
-    assert raw_edf.info['sfreq'] == config.SAMPLING_FREQUENCY  # type: ignore
-    data = raw_edf.get_data()  # type: ignore
-    return data.astype(np.float32) * 1e6  # type: ignore
+def read_raw_edf(edf_path: pathlib.Path) -> mne.io.Raw:
+    raw_edf = mne.io.read_raw_edf(edf_path, stim_channel=None, include=config.USEFUL_CHANNELS, verbose="ERROR")  # type: ignore
+
+    if "T8-P8-1" in raw_edf.ch_names:
+        raw_edf.drop_channels("T8-P8-1")
+        new_names = {"T8-P8-0": "T8-P8"}
+        raw_edf.rename_channels(new_names)
+
+    raw_edf.reorder_channels(config.USEFUL_CHANNELS)
+    return raw_edf  # type: ignore
 
 
-def load_data(edf_path: pathlib.Path, segments) -> tuple[np.ndarray, np.ndarray]:
-    data = extract_data(edf_path)
-    assert data.shape[0] == len(config.USEFUL_CHANNELS)
-    print(data.shape)
-    return
-    X = []
+def extract_data(raw: mne.io.Raw) -> np.ndarray:
+    raw = raw.load_data()
+    data = raw.filter(8, 13).get_data().as_type(np.float32) * 1e6  # type: ignore
+    return data
+
+
+def split_ictals(raw_edf: mne.io.Raw, segments: list) -> tuple[list[mne.io.Raw], list[mne.io.Raw]]:
+    interictals = []
+    preictals = []
     for start, end, contains_seizure in segments:
-        X.append(data[:, start * config.SAMPLING_FREQUENCY:end * config.SAMPLING_FREQUENCY])
-    X_array = np.concatenate(X)
+        if contains_seizure:
+            preictal_start = min(start, end - config.PREICTAL_SECONDS)
+            preictals.append(raw_edf.crop(preictal_start, end))
+            if preictal_start > start:
+                interictals.append(raw_edf.crop(start, preictal_start))
+        else:
+            interictals.append(raw_edf.crop(start, end))
+    return interictals, preictals
+
+
+def split_epochs(edfs: list[mne.io.Raw], duration: int, overlap: int) -> list[mne.io.Raw]:
+    epochs_groups = []
+    for edf in edfs:
+        epochs = mne.make_fixed_length_epochs(edf, duration=duration, overlap=overlap)
+        epochs_groups.append(epochs)
+    return epochs_groups
+
+
+def extract_data_and_labels(edf_path: pathlib.Path, segments: list) -> tuple[list[np.ndarray], list[np.ndarray]]:
+    raw_edf = read_raw_edf(edf_path)
+    interictals, preictals = split_ictals(raw_edf, segments)
+
+    interictal_epochs_groups = split_epochs(interictals, config.WINDOW_SIZE_SECONDS, config.WINDOW_OVERLAP_SECONDS)
+    interictal_data = []
+    for interictal_epochs in interictal_epochs_groups:
+        interictal_data.append(extract_data(interictal_epochs))
+
+    preictal_epochs_groups = split_epochs(preictals, config.WINDOW_SIZE_SECONDS, config.WINDOW_OVERLAP_SECONDS)
+    preictal_data = []
+    for interictal_epochs in preictal_epochs_groups:
+        preictal_data.append(extract_data(interictal_epochs))
+
+    return interictal_data, preictal_data
 
 
 def main():
@@ -56,6 +83,8 @@ def main():
     slices_metadata = load_slices_metadata(output_path)
     for patient, patient_slices in slices_metadata.items():
         for edf_filename, segments in patient_slices.items():
+            if edf_filename in config.DISCARDED_EDFS:
+                continue
             edf_path = dataset_path.joinpath(patient, edf_filename)
             X, y = load_data(edf_path, segments)
             return
