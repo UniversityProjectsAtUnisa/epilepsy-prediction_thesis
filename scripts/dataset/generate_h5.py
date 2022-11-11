Fixed ram overflow

scripts/dataset/generate_h5.py
@@ -5,6 +5,7 @@ import pathlib
 import mne
 import numpy as np
 from tqdm import tqdm
+from utils import ResizableH5Dataset
 
 
 def other_files(output_path, filter_out):
@@ -91,19 +92,19 @@ def main():
 
     slices_metadata = load_slices_metadata(output_path)
     for patient, patient_slices in tqdm(slices_metadata.items(), desc="Patients"):
-        interictal_data, preictal_data = [], []
-        for edf_filename, segments in tqdm(patient_slices.items(), leave=False, desc=f"Patient {patient}"):
-            if edf_filename in config.DISCARDED_EDFS:
-                continue
-            edf_path = dataset_path.joinpath(patient, edf_filename)
-            i, p = extract_data_and_labels(edf_path, segments)
-            interictal_data.extend(i)
-            preictal_data.extend(p)
         with h5py.File(output_path.joinpath(config.H5_FILENAME), "a") as f:
-            f.create_dataset(f"{patient}/normal", data=np.concatenate(interictal_data))
-            for i, preictal in enumerate(preictal_data):
-                f.create_dataset(f"{patient}/anomaly/{i}", data=preictal)
-        del interictal_data, preictal_data
+            anomalies = 0
+            normal_dataset = ResizableH5Dataset(f"{patient}/normal")
+            for edf_filename, segments in tqdm(patient_slices.items(), leave=False, desc=f"Patient {patient}"):
+                if edf_filename in config.DISCARDED_EDFS:
+                    continue
+                edf_path = dataset_path.joinpath(patient, edf_filename)
+                interictal_windows, preictal_windows = extract_data_and_labels(edf_path, segments)
+                if interictal_windows:
+                    normal_dataset.append_data(f, data=np.concatenate(interictal_windows))
+                for p in preictal_windows:
+                    f.create_dataset(f"{patient}/anomaly/{anomalies}", data=p)
+                    anomalies += 1
 
 
 if __name__ == '__main__':

scripts/dataset/utils.py
@@ -1,11 +1,34 @@
 import os
-# a function to get an environment variable that throws an exception if not found
+import h5py
+import numpy as np
+import typing
 
 
 def get_envvar(name, default: str | None = None):
+    """a function to get an environment variable that throws an exception if not found"""
     value = os.getenv(name)
     if value is not None:
         return value
     if default is not None:
         return default
     raise ValueError(f"Environment variable `{name}` not found")
+
+
+class ResizableH5Dataset:
+    def __init__(self, h5_path: str, compression='lzf'):
+        self.h5_path = h5_path
+        self.compression = compression
+        self._created = False
+
+    def append_data(self, h5_file: h5py.File, data: np.ndarray):
+        if not self._created:
+            sample_shape = data.shape[1:]
+            kwargs = dict(maxshape=(None, *sample_shape), dtype=data.dtype, compression=self.compression)
+            ds = h5_file.create_dataset(self.h5_path, shape=data.shape, **kwargs)
+            ds[:] = data
+            self._created = True
+        else:
+            ds = typing.cast(h5py.Dataset, h5_file[self.h5_path])
+            curr_size = ds.shape[0]
+            ds.resize(curr_size + data.shape[0], axis=0)
+            ds[curr_size:] = data